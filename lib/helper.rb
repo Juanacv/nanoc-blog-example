e459635172d03e0a92da13770452f1491fd6ac8e
Juan Antonio <juan.acv@gmail.com>
2015-08-13 21:37:42 +0200

    Added 'Continue Reading' I forgot

--- lib/helper.rb
+++ lib/helper.rb
@@ -12,6 +12,16 @@ module PostHelper
     	attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
     end
   end
+  
+  def get_post_start(post)
+    content = post.compiled_content
+    if content =~ /\s<!-- more -->\s/
+      content = content.partition('<!-- more -->').first +
+      "<div class='read-more'><a href='#{post.path}'>Continue reading &rsaquo;</a></div>"
+    end
+   return content
+  end
+
 end
 
 include PostHelper
